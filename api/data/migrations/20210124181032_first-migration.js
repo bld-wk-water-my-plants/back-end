05c172e5d369d1a58aad88166a599753deffe1b7
ST1414 <sterry1414@gmail.com>
2021-12-18 19:39:50 -0800

    Update the migration file to build 3 tables schemas

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,41 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable('users', (table) => {
+      table.increments('user_id')
+      table.string('username', 200).notNullable().unique()
+      table.string('password', 200).notNullable()
+      table.string('phone_number', 200).notNullable().unique()
+      // users.timestamps(false, true)
+    })
+    .createTable('species', (table) => {
+      table.increments('species_id')
+      table.string('species_name', 200).notNullable().unique()
+      table.integer('h2o_frequency', 200).notNullable().unsigned()
+      table.string('image', 200)
+      // species.timestamps(false, true)
+    })
+    .createTable('plants', (table) => {
+      table.increments('plant_id')
+      table.string('plant_nickname', 200).notNullable() // <<< Unique ???
+      table.string('user_id', 200)
+        .notNullable()
+        .unsigned()
+        .references('user_id')
+        .inTable('users')
+        .onDelete('RESTRICT')
+      table.string('species_id', 200)
+        .notNullable()
+        .unsigned()
+        .references('species_id')
+        .inTable('species')
+        .onDelete('RESTRICT')
+      // plants.timestamps(false, true)
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+    .dropTableIfExists('plants')
+    .dropTableIfExists('species')
+    .dropTableIfExists('users')
 }
