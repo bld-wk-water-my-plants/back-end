2d41459c56f7e440ae11685a0d1bd7ca41d58a24
ST1414 <sterry1414@gmail.com>
2021-12-18 20:45:49 -0800

    added commas to species seed file

--- api/data/seeds/03-species.js
+++ api/data/seeds/03-species.js
@@ -4,9 +4,9 @@
 // Baking flour
 exports.seed = async function (knex) {
     await knex('species').insert([
-        {species_name:'Acer palmatum', h2o_frequency:1, image:'https://images.unsplash.com/photo-1618172449507-8afee268f7b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80' }
-        {species_name:'Hydrangea macrophylla', h2o_frequency:1, image:'https://images.pexels.com/photos/7336208/pexels-photo-7336208.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' }
-        {species_name:'Hesperrhodos', h2o_frequency:1, image:'https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+        {species_name:'Acer palmatum', h2o_frequency:1, image:'https://images.unsplash.com/photo-1618172449507-8afee268f7b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80' },
+        {species_name:'Hydrangea macrophylla', h2o_frequency:1, image:'https://images.pexels.com/photos/7336208/pexels-photo-7336208.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
+        {species_name:'Hesperrhodos', h2o_frequency:1, image:'https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
         {species_name:'Cornerfolorus powderdus', h2o_frequency:4, image:'https://images.pexels.com/photos/6605210/pexels-photo-6605210.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
     ])
 }
--- api/data/seeds/04-plants.js
+++ api/data/seeds/04-plants.js
@@ -19,6 +19,6 @@ exports.seed = async function (knex) {
         {plant_nickname: 'Coral Bark Maple', user_id: '4', species_id:'1' },
         {plant_nickname: 'Hydrangea', user_id: '4', species_id:'2' },
         {plant_nickname: 'Rose', user_id: '4', species_id:'3' },
-        {plant_nickname: 'Baking Flour', user_id: '4', species_id:'4' },
+        {plant_nickname: 'Baking Flour', user_id: '4', species_id:'4' }
     ])
 }
